8d9eb37245bfb87c26778a924ce29c4582c7739c
chejarlanaveen <127005558+chejarlanaveen@users.noreply.github.com>
2025-03-31 03:00:39 +0530

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,8 @@
+function changebgcolor(){
+    let colour=document.getElementById("colorname").value;
+    document.body.style.backgroundColor=colour;
+}
+
+function changebgcolour(colour){
+    document.body.style.backgroundColor=colour;
+}
